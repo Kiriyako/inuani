8b93c66dbc8de9924da5cc21a50f60c981f02d86
Kiriyako <doraemonboy158@gmail.com>
2023-06-25 18:55:48 +0530

    fixed search

--- src/app/search/[name]/page.js
+++ src/app/search/[name]/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default async function Search({ params }) {
-  const query = params.name;
+  const query = decodeURIComponent(params.name);
 
   async function getSearchData() {
     const res = await fetch(`https://api.enime.moe/search/${encodeURIComponent(query)}`, { cache: 'no-store' });
@@ -9,17 +9,22 @@ export default async function Search({ params }) {
   }
 
   const data = await getSearchData();
+  const hasData = data && data.data && Array.isArray(data.data) && data.data.length > 0;
 
   return (
     <div id="animegrid">
-      {data.data.map((anime) => (
-        <Link href={`/anime/${anime.id}`} key={anime.id}>
-          <div id="anime">
-            <img width="250" height="350" alt={anime.title.romaji} src={anime.coverImage} />
-            <h2 style={{ color: anime.color }}>{anime.title.romaji}</h2>
-          </div>
-        </Link>
-      ))}
+      {hasData ? (
+        data.data.map((anime) => (
+          <Link href={`/anime/${anime.id}`} key={anime.id}>
+            <div id="anime">
+              <img width="250" height="350" alt={anime.title.romaji} src={anime.coverImage} />
+              <h2 style={{ color: anime.color }}>{anime.title.romaji}</h2>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <div>No results found</div>
+      )}
     </div>
   );
 }
