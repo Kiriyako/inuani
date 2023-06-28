62db531014bf13a527d8f116393d54502cc2a702
Kiriyako <doraemonboy158@gmail.com>
2023-06-28 11:08:05 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar/nav'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 import Footer from './components/Footer/footer'
+import Script from 'next/script'
 const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export const metadata = {
@@ -13,7 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Navbar />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-H2CPPMFKCF">
+
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', newDate());
+          gtag('config', 'G-H2CPPMFKCF')
+          `}
+        
+        </Script>
+         <Navbar />
         {children}
         <Footer />
         </body>
