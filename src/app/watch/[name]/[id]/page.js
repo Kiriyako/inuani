04cb3e1efedc5261890a8d95cdeb2239399dade8
Kiriyako <doraemonboy158@gmail.com>
2023-07-26 13:58:16 +0530

    improved player (still working on progress bar)

--- src/app/watch/[name]/[id]/page.js
+++ src/app/watch/[name]/[id]/page.js
@@ -70,10 +70,23 @@ export default function AnimePage({ params }) {
         playerInstance.use([
           ui({
             theme: {
-              primaryColor: "rgb(255, 156, 181)",
+              primaryColor: "rgb(64, 179, 255)",
             },
             controlBar: { back: "fullscreen" },
-            icons: {},
+            icons: {
+              play: `<svg viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>`,
+              pause: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>`,
+              volume: [
+                `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/></svg>`,
+                `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><line x1="22" x2="16" y1="9" y2="15"/><line x1="16" x2="22" y1="9" y2="15"/></svg>`
+              ],
+              fullscreen: [
+                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>`,
+                `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>`
+              ],
+              loop: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-repeat-2"><path d="m2 9 3-3 3 3"/><path d="M13 18H7a2 2 0 0 1-2-2V6"/><path d="m22 15-3 3-3-3"/><path d="M11 6h6a2 2 0 0 1 2 2v10"/></svg>`,
+              playbackRate: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gauge"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>`,
+            },
             keyboard: { focused: true, global: false },
             showControls: "always",
             miniProgressBar: true,
@@ -89,20 +102,7 @@ export default function AnimePage({ params }) {
         playerInstance.create();
         playerRef.current = playerInstance;
 
-        playerInstance.context.ui.menu.register({
-          name: "QUALITY",
-          children: watchData.sources.map((source) => ({
-            name: source.quality,
-            default: source.quality === "default",
-            value: source.url,
-          })),
-          onChange({ name, value }, elm) {
-            elm.innerText = name;
-            setSelectedQuality(name);
-            setVideoSource(value);
-            playerInstance.changeSource({ src: value });
-          },
-        });
+       
 
         const matchingEpisode = episodeData.find((ep) => ep.id === watch);
         if (matchingEpisode) {
