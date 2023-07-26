7e2b108534ab7d6b310f93acdea0c028ad13c85d
Kiriyako <doraemonboy158@gmail.com>
2023-07-26 15:27:46 +0530

    fixed video player

--- src/app/watch/[name]/[id]/page.js
+++ src/app/watch/[name]/[id]/page.js
@@ -47,32 +47,27 @@ export default function AnimePage({ params }) {
       const data = await res.json();
       return data;
     }
-
     Promise.all([getEpisodeData(), getEpisodeWatch(), getAniData()]).then(
       ([episodeData, watchData, aniData]) => {
-        console.log(episodeData);
-        console.log(watchData);
-        console.log(aniData);
-
         setEpisodes(episodeData);
         setAniData(aniData);
         setMatchingTitle(episodeData[0]?.title || "");
         setMatchingEpisodeNumber(episodeData[0]?.number || "");
 
-        const playerInstance = Player.make("#app", {
-          isLive: true,
+        const player = Player.make("#app", {
           source: {
-            src: "", 
-            title: matchingTitle,
+            src: "",
           },
-        });
-
-        playerInstance.use([
+          videoAttr: {
+            // crossOrigin: "anonymous"
+          }
+        }).use([
           ui({
             theme: {
-              primaryColor: "rgb(64, 179, 255)",
+              primaryColor: "rgb(117, 171, 255)",
             },
-            controlBar: { back: "fullscreen" },
+            screenshot: true,
+            controlBar: { back: "always" },
             icons: {
               play: `<svg viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>`,
               pause: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>`,
@@ -86,44 +81,58 @@ export default function AnimePage({ params }) {
               ],
               loop: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-repeat-2"><path d="m2 9 3-3 3 3"/><path d="M13 18H7a2 2 0 0 1-2-2V6"/><path d="m22 15-3 3-3-3"/><path d="M11 6h6a2 2 0 0 1 2 2v10"/></svg>`,
               playbackRate: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gauge"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>`,
-            },
-            keyboard: { focused: true, global: false },
-            showControls: "always",
-            miniProgressBar: true,
-            slideToSeek: "always",
-            screenshot: true,
-            autoFocus: true,
-            forceLandscapeOnFullscreen: true,
-            progressIndicator: true,
+            }
           }),
           hls({ forceHLS: true }),
         ]);
 
-        playerInstance.create();
-        playerRef.current = playerInstance;
+        player.create();
 
-       
+        var forward = document.createElement("button");
+        forward.className = "forward";
+        forward.innerHTML =
+          '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.444 3.685A10 10 0 0 1 18 4h-2v2h4a1 1 0 0 0 1-1V1h-2v1.253A12 12 0 1 0 24 12h-2A10 10 0 1 1 6.444 3.685ZM22 4v3h-3v2h4a1 1 0 0 0 1-1V4h-2Zm-9.398 11.576c.437.283.945.424 1.523.424s1.083-.141 1.513-.424c.437-.29.774-.694 1.009-1.215.235-.527.353-1.148.353-1.861 0-.707-.118-1.324-.353-1.851-.235-.527-.572-.932-1.009-1.215-.43-.29-.935-.434-1.513-.434-.578 0-1.086.145-1.523.434-.43.283-.764.688-.999 1.215-.235.527-.353 1.144-.353 1.851 0 .713.118 1.334.353 1.86.236.522.568.927.999 1.216Zm2.441-1.485c-.222.373-.528.56-.918.56s-.696-.187-.918-.56c-.222-.38-.333-.91-.333-1.591 0-.681.111-1.208.333-1.581.222-.38.528-.57.918-.57s.696.19.918.57c.222.373.333.9.333 1.581 0 .681-.111 1.212-.333 1.59Zm-6.439-3.375v5.14h1.594V9.018L7 9.82v1.321l1.604-.424Z" fill="currentColor"></path></svg>';
+        forward.onclick = function () {
+          player.seek(player.currentTime + 10);
+        };
+
+        var backward = document.createElement("button");
+        backward.className = "backward";
+        backward.innerHTML =
+          '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.02 2.048A10 10 0 1 1 2 12H0a12 12 0 1 0 5-9.747V1H3v4a1 1 0 0 0 1 1h4V4H6a10 10 0 0 1 5.02-1.952ZM2 4v3h3v2H1a1 1 0 0 1-1-1V4h2Zm12.125 12c-.578 0-1.086-.141-1.523-.424-.43-.29-.764-.694-.999-1.215-.235-.527-.353-1.148-.353-1.861 0-.707.118-1.324.353-1.851.236-.527.568-.932.999-1.215.437-.29.945-.434 1.523-.434s1.083.145 1.513.434c.437.283.774.688 1.009 1.215.235.527.353 1.144.353 1.851 0 .713-.118 1.334-.353 1.86-.235.522-.572.927-1.009 1.216-.43.283-.935.424-1.513.424Zm0-1.35c.39 0 .696-.186.918-.56.222-.378.333-.909.333-1.59s-.111-1.208-.333-1.581c-.222-.38-.528-.57-.918-.57s-.696.19-.918.57c-.222.373-.333.9-.333 1.581 0 .681.111 1.212.333 1.59.222.374.528.56.918.56Zm-5.521 1.205v-5.139L7 11.141V9.82l3.198-.8v6.835H8.604Z" fill="currentColor"></path></svg>';
+        backward.onclick = function () {
+          player.seek(player.currentTime - 10);
+        };
+
+        player.$root.appendChild(backward);
+        player.$root.appendChild(forward);
 
         const matchingEpisode = episodeData.find((ep) => ep.id === watch);
         if (matchingEpisode) {
           setMatchingTitle(matchingEpisode.title);
           setMatchingEpisodeNumber(matchingEpisode.number);
           if (watchData.sources && watchData.sources.length > 0) {
-            setVideoSource(watchData.sources[watchData.sources.length - 1].url);
-            playerInstance.changeSource({ src: watchData.sources[watchData.sources.length - 1].url });
+            setVideoSource(
+              watchData.sources[watchData.sources.length - 1].url
+            );
+            player.changeSource({
+              src: watchData.sources[watchData.sources.length - 1].url
+            });
           }
         }
 
         setIsPlayerReady(true);
+        playerRef.current = player;
       }
     );
 
     return () => {
       if (playerRef.current) {
-        playerRef.current.destroy(); 
+        playerRef.current.destroy();
       }
     };
   }, [params]);
+
 
   return (
     <div id="main">
