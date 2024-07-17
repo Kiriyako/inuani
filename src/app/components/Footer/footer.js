4dbf429dd5c3451322d8ac304d96d116c1f46b21
Kiriyako <doraemonboy158@gmail.com>
2024-07-17 13:20:47 +0530

    yearly update

--- src/app/components/Footer/footer.js
+++ src/app/components/Footer/footer.js
@@ -1,4 +1,4 @@
 export default function Footer(){
-    return <div id="footer"><hr></hr>Made with ❤️ by Kiriyako<br></br> Disclaimer: This site is NOT affiliated with the anime studios responsible for creating the content displayed, and is not endorsed by them. All content is provided by independent third-party services.
+    return <div id="footer"><hr></hr>Disclaimer: This site is NOT affiliated with the anime studios responsible for creating the content displayed, and is not endorsed by them. All content is provided by independent third-party services.
     </div>
 }
--- src/app/watch/[name]/[id]/page.js
+++ src/app/watch/[name]/[id]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Player from "@oplayer/core";
 import ui from "@oplayer/ui";
 import hls from "@oplayer/hls";
@@ -11,135 +11,110 @@ export default function AnimePage({ params }) {
   const [episodes, setEpisodes] = useState([]);
   const [animeData, setAnimeData] = useState(null);
   const [videoSource, setVideoSource] = useState("");
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-  const playerRef = useRef(null);
+  const [player, setPlayer] = useState(null);
+
   const findEpisodeNumber = (episodeId) => {
     const episode = animeData?.episodes.find((ep) => ep.id === episodeId);
     return episode ? episode.number : "Unknown";
   };
+
   useEffect(() => {
-    async function getEpisodeData() {
+    async function fetchData() {
       try {
-        const res = await fetch(
-          `https://api-consumet-org-gamma-sage.vercel.app/anime/gogoanime/watch/${watch}`,
-          { cache: "no-store" }
-        );
-        const data = await res.json();
-        return data;
+        const episodeRes = await fetch(`https://api-consumet-org-gamma-sage.vercel.app/anime/gogoanime/watch/${watch}`, { cache: "no-store" });
+        const episodeData = await episodeRes.json();
+        const animeRes = await fetch(`https://api-consumet-org-gamma-sage.vercel.app/anime/gogoanime/info/${anime}`, { cache: "no-store" });
+        const animeData = await animeRes.json();
+
+        setEpisodes(episodeData);
+        setAnimeData(animeData);
       } catch (error) {
-        console.error("Error fetching episode data:", error);
-        return {};
+        console.error("Error fetching data:", error);
       }
     }
 
-    async function getAniData() {
-      try {
-        const res = await fetch(
-          `https://api-consumet-org-gamma-sage.vercel.app/anime/gogoanime/info/${anime}`,
-          { cache: "no-store" }
-        );
-        const data = await res.json();
-        setAnimeData(data); // Move this line here
-        return data;
-      } catch (error) {
-        console.error("Error fetching anime data:", error);
-        return {};
-      }
-    }
-
-    Promise.all([getEpisodeData(), getAniData()]).then(([episodeData]) => {
-      const player = Player.make("#app", {
-        source: {
-          src: "",
-        },
-        defaultQuality: "1080p",
-        videoAttr: {
-          // crossOrigin: "anonymous"
-        },
-      }).use([
-        ui({
-          theme: {
-            primaryColor: "rgb(231 170 227)",
-          },
-          screenshot: true,
-          controlBar: { back: "always" },
-          icons: {
-            play: `<svg viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>`,
-            pause: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>`,
-            volume: [
-              `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/></svg>`,
-              `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><line x1="22" x2="16" y1="9" y2="15"/><line x1="16" x2="22" y1="9" y2="15"/></svg>`,
-            ],
-            fullscreen: [
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>`,
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>`,
-            ],
-            loop: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-repeat-2"><path d="m2 9 3-3 3 3"/><path d="M13 18H7a2 2 0 0 1-2-2V6"/><path d="m22 15-3 3-3-3"/><path d="M11 6h6a2 2 0 0 1 2 2v10"/></svg>`,
-            playbackRate: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gauge"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>`,
-          },
-        }),
-        hls({ forceHLS: true }),
-      ]);
-
-      player.create();
-      setEpisodes(episodeData);
-
-      var forward = document.createElement("button");
-      forward.className = "forward";
-      forward.innerHTML =
-        '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M6.444 3.685A10 10 0 0 1 18 4h-2v2h4a1 1 0 0 0 1-1V1h-2v1.253A12 12 0 1 0 24 12h-2A10 10 0 1 1 6.444 3.685ZM22 4v3h-3v2h4a1 1 0 0 0 1-1V4h-2Zm-9.398 11.576c.437.283.945.424 1.523.424s1.083-.141 1.513-.424c.437-.29.774-.694 1.009-1.215.235-.527.353-1.148.353-1.861 0-.707-.118-1.324-.353-1.851-.235-.527-.572-.932-1.009-1.215-.43-.29-.935-.434-1.513-.434-.578 0-1.086.145-1.523.434-.43.283-.764.688-.999 1.215-.235.527-.353 1.144-.353 1.851 0 .713.118 1.334.353 1.86.236.522.568.927.999 1.216Zm2.441-1.485c-.222.373-.528.56-.918.56s-.696-.187-.918-.56c-.222-.38-.333-.91-.333-1.591 0-.681.111-1.208.333-1.581.222-.38.528-.57.918-.57s.696.19.918.57c.222.373.333.9.333 1.581 0 .681-.111 1.212-.333 1.59Zm-6.439-3.375v5.14h1.594V9.018L7 9.82v1.321l1.604-.424Z" fill="currentColor"></path></svg>';
-      forward.onclick = function () {
-        player.seek(player.currentTime + 10);
-      };
-
-      var backward = document.createElement("button");
-      backward.className = "backward";
-      backward.innerHTML =
-        '<svg viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.02 2.048A10 10 0 1 1 2 12H0a12 12 0 1 0 5-9.747V1H3v4a1 1 0 0 0 1 1h4V4H6a10 10 0 0 1 5.02-1.952ZM2 4v3h3v2H1a1 1 0 0 1-1-1V4h2Zm12.125 12c-.578 0-1.086-.141-1.523-.424-.43-.29-.764-.694-.999-1.215-.235-.527-.353-1.148-.353-1.861 0-.707.118-1.324.353-1.851.236-.527.568-.932.999-1.215.437-.29.945-.434 1.523-.434s1.083.145 1.513.434c.437.283.774.688 1.009 1.215.235.527.353 1.144.353 1.851 0 .713-.118 1.334-.353 1.86-.235.522-.572.927-1.009 1.216-.43.283-.935.424-1.513.424Zm0-1.35c.39 0 .696-.186.918-.56.222-.378.333-.909.333-1.59s-.111-1.208-.333-1.581c-.222-.38-.528-.57-.918-.57s-.696.19-.918.57c-.222.373-.333.9-.333 1.581 0 .681.111 1.212.333 1.59.222.374.528.56.918.56Zm-5.521 1.205v-5.139L7 11.141V9.82l3.198-.8v6.835H8.604Z" fill="currentColor"></path></svg>';
-      backward.onclick = function () {
-        player.seek(player.currentTime - 10);
-      };
-
-      player.$root.appendChild(backward);
-      player.$root.appendChild(forward);
-
-      if (episodeData.sources && episodeData.sources.length > 0) {
-        setVideoSource(episodeData.sources[episodeData.sources.length - 1].url);
-        player.changeSource({
-          src: episodeData.sources[episodeData.sources.length - 1].url,
-        });
-      }
-
-      setIsPlayerReady(true);
-      playerRef.current = player;
-    });
+    fetchData();
 
     return () => {
-      if (playerRef.current) {
-        playerRef.current.destroy();
+      if (player && typeof player.destroy === 'function') {
+        player.destroy();
       }
     };
-  }, [params]);
+  }, [anime, watch]);
+
+  useEffect(() => {
+    if (!animeData || episodes.length === 0) return;
+
+    const newPlayer = Player.make("#app", {
+      source: { src: videoSource },
+      defaultQuality: "1080p",
+      videoAttr: {
+        // crossOrigin: "anonymous"
+      },
+    }).use([
+      ui({
+        theme: {
+          primaryColor: "rgb(231 170 227)",
+        },
+        controlBar: { back: "always" },
+        icons: {
+          play: `<svg viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"/></svg>`,
+          pause: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pause"><rect width="4" height="16" x="6" y="4"/><rect width="4" height="16" x="14" y="4"/></svg>`,
+          volume: [
+            `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-2"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><path d="M15.54 8.46a5 5 0 0 1 0 7.07"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14"/></svg>`,
+            `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume-x"><polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"/><line x1="22" x2="16" y1="9" y2="15"/><line x1="16" x2="22" y1="9" y2="15"/></svg>`,
+          ],
+          fullscreen: [
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>`,
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>`,
+          ],
+          loop: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-repeat-2"><path d="m2 9 3-3 3 3"/><path d="M13 18H7a2 2 0 0 1-2-2V6"/><path d="m22 15-3 3-3-3"/><path d="M11 6h6a2 2 0 0 1 2 2v10"/></svg>`,
+          playbackRate: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-gauge"><path d="m12 14 4-4"/><path d="M3.34 19a10 10 0 1 1 17.32 0"/></svg>`,
+        },
+      }),
+      hls({ forceHLS: true }),
+    ]);
+
+    newPlayer.create();
+
+    if (episodes.sources && episodes.sources.length > 0) {
+      setVideoSource(episodes.sources[episodes.sources.length - 1].url);
+      newPlayer.changeSource({
+        src: episodes.sources[episodes.sources.length - 1].url,
+      });
+    }
+
+    setPlayer(newPlayer);
+
+    return () => {
+      if (newPlayer && typeof newPlayer.destroy === 'function') {
+        newPlayer.destroy();
+      }
+    };
+  }, [animeData, episodes]);
+
+  if (!animeData) {
+    return <div>Now Loading...</div>;
+  }
 
   return (
     <div id="main">
       <div id="app"></div>
-
-      <text id="animetitle">
-        {" "}
-        <Link href={`/anime/${animeData?.id}`}>{animeData?.title}</Link> </text> <br></br> <text id="episodetitle"> Episode{" "}
-        {findEpisodeNumber(watch)}
-      </text>
+      <text id="animetitle">Episode {findEpisodeNumber(watch)}</text>
+      <br></br>
+      <text id="episodetitle">
+        <Link href={`/anime/${animeData.id}`}>{animeData.title}</Link>
+      </text> 
 
       <div id="episodes">
-        <h2>Episodes ({animeData?.totalEpisodes})</h2>
+        <h2>Episodes ({animeData.totalEpisodes})</h2>
         <div className="episodelist-container">
           <div id="episodelist" className="scroll-x">
-            {animeData?.episodes.map((ep) => (
+            {animeData.episodes.map((ep) => (
               <div className="episode-box" key={ep.id}>
-                <a href={`/watch/${anime}/${ep.id}`} rel="noopener noreferrer">
+                <Link href={`/watch/${anime}/${ep.id}`} rel="noopener noreferrer">
                   <h2 className="episode-title">{ep.number}</h2>
-                </a>
+                </Link>
               </div>
             ))}
           </div>
