3d97456387733a85f360c730eb84efc7290c84f0
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-04-01 09:02:19 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -39,8 +39,6 @@ export default async function Page() {
           </Link>
         ))}
       </div>
-          <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
-<script>eruda.init();</script>
     </div>
   );
 }
