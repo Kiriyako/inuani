1c746b53c2db671d461a524b9aaaf822e3478636
anime-kun32 <alabiadedoyinjohn@protonmail.com>
2025-04-02 14:44:08 +0100

    Update loading.js

--- src/app/watch/[name]/[id]/[number]/loading.js
+++ src/app/watch/[name]/[id]/[number]/loading.js
@@ -1,17 +1,3 @@
-"use client";
-import { useEffect } from "react";
-
-export default function Load() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/npm/eruda";
-    script.async = true;
-    script.onload = () => {
-      // Initialize Eruda after script loads
-      window.eruda.init();
-    };
-    document.body.appendChild(script);
-  }, []);
-
-  return <div id="loading">Now Loading...</div>;
+export default function Load(){
+    return <div id="loading">Now Loading...</div>
 }
